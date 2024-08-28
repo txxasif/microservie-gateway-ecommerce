ec9c4bc0b1d505c52c88177787008239a89daad5
Asif <txxasif@gmail.com>
2024-08-28 21:07:12 +0600

    auth services

--- src/server.ts
+++ src/server.ts
@@ -26,6 +26,7 @@ import { config } from '@gateway/config';
 import { isAxiosError } from 'axios';
 import { elasticSearch } from '@gateway/elasticsearch';
 import { appRoutes } from '@gateway/routes';
+import { axiosAuthInstance } from '@gateway/services/api/auth.service';
 
 const SERVER_PORT = 4000;
 const DEFAULT_ERROR_CODE = 500;
@@ -71,18 +72,18 @@ export class GatewayServer {
       })
     );
 
-    // app.use((req: Request, _res: Response, next: NextFunction) => {
-    //   if (req.session?.jwt) {
-    //     axiosAuthInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
-    //     axiosBuyerInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
-    //     axiosSellerInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
-    //     axiosGigInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
-    //     axiosMessageInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
-    //     axiosOrderInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
-    //     axiosReviewInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
-    //   }
-    //   next();
-    // });
+    app.use((req: Request, _res: Response, next: NextFunction) => {
+      if (req.session?.jwt) {
+        axiosAuthInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
+        // axiosBuyerInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
+        // axiosSellerInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
+        // axiosGigInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
+        // axiosMessageInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
+        // axiosOrderInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
+        // axiosReviewInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
+      }
+      next();
+    });
   }
 
   private standardMiddleware(app: Application): void {
