81668ed0e3ffb70273ec13275a670d55f9ac6632
Asif <txxasif@gmail.com>
2024-09-29 21:26:33 +0600

    current-user

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,9 @@
 import { healthRoutes } from '@gateway/routes/health';
 import { Application } from 'express';
+import { authRoutes } from '@gateway/routes/auth';
 
+const BASE_PATH = '/api/gateway/v1';
 export const appRoutes = (app: Application) => {
   app.use('', healthRoutes.routes());
+  app.use(BASE_PATH, authRoutes.routes());
 };
